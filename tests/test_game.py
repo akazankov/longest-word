218e8f8abef05669cefaf5a4cd8a2baa1ae7481e
Alexey Kazankov <akazankov@auchan.com>
2022-07-26 15:15:01 +0200

    TDD: Check that attempt exists in the English dictionary

--- tests/test_game.py
+++ tests/test_game.py
@@ -11,3 +11,24 @@ class TestGame(unittest.TestCase):
         self.assertEqual(len(grid), 9)
         for letter in grid:
             self.assertIn(letter, string.ascii_uppercase)
+            
+    def test_empty_word_is_invalid(self):
+        new_game = Game()
+        self.assertIs(new_game.is_valid(''), False)
+
+    def test_is_valid(self):
+        new_game = Game()
+        new_game.grid = list('KWEUEAKRZ') # Force the grid to a test case:
+        self.assertIs(new_game.is_valid('EUREKA'), True)
+        self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # Make sure the grid remained untouched
+
+    def test_is_invalid(self):
+        new_game = Game()
+        new_game.grid = list('KWEUEAKRZ') # Force the grid to a test case:
+        self.assertIs(new_game.is_valid('SANDWICH'), False)
+        self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # Make sure the grid remained untouched
+    
+    def test_unknown_word_is_invalid(self):
+      new_game = Game()
+      new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+      self.assertIs(new_game.is_valid('FEUN'), False)
